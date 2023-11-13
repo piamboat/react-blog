dd5941fea9039ed3e7fe8b9d35298588fe5868f0
piamsak <piamsakboat@gmail.com>
2023-11-13 15:38:22 +0700

    add create button

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,50 +1,62 @@
-import NavMain from "../components/NavMain"
+import NavMain from "../components/NavMain";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserInterface } from "../interfaces/user.interface";
 import UserCard from "../components/UserCard";
 
 const User: React.FC = () => {
-    const navigate = useNavigate();
-    const [user, setUser] = useState([]);
-  
-    useEffect(() => {
-      const getUsers = async () => {
-        try {
-          const token = localStorage.getItem("authToken");
-  
-          const response = await fetch("http://localhost:3000/api/v1/user", {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          });
-  
-          if (response.ok) {
-            const data = await response.json();
-            setUser((prev) => data);
-          } else {
-            navigate("/");
-          }
-        } catch (error) {
+  const navigate = useNavigate();
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const token = localStorage.getItem("authToken");
+
+        const response = await fetch("http://localhost:3000/api/v1/user", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setUser((prev) => data);
+        } else {
           navigate("/");
         }
-      };
-  
-      getUsers();
-    }, []);
-  
-    return (
-      <>
-        <NavMain />
-        <div className="container">
-          {user.map((u: UserInterface) => (
-            <UserCard key={u.id} name={u.name} email={u.email} phone={u.phone} address={u.address} company={u.company}/>
-          ))}
-        </div>
-      </>
-    );
-}
+      } catch (error) {
+        navigate("/");
+      }
+    };
 
-export default User
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <NavMain />
+      <div className="fixed-button-container">
+        <div className="round-button">
+            +
+        </div>
+      </div>
+      <div className="container">
+        {user.map((u: UserInterface) => (
+          <UserCard
+            key={u.id}
+            name={u.name}
+            email={u.email}
+            phone={u.phone}
+            address={u.address}
+            company={u.company}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default User;
