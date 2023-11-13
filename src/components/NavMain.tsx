17c0d1a4b52073942cd106e2a6c855f8ee9a1f2c
piamsak <piamsakboat@gmail.com>
2023-11-14 06:37:09 +0700

    fix nav menu

--- src/components/NavMain.tsx
+++ src/components/NavMain.tsx
@@ -1,16 +1,18 @@
-import {Link} from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
 const NavMain: React.FC = () => {
-    return (
-        <nav>
-            <div className="logo">SnakeBlog</div>
-            <div className="nav-link">
-                <Link to="/">Home</Link>
-                <Link to="/user">User</Link>
-                <Link to="/post">Post</Link>
-            </div>
-        </nav>
-    )
-}
+  const location = useLocation();
+  return (
+    <nav>
+      <div className="logo">SnakeBlog</div>
+      {location.pathname != "/" && (
+        <div className="nav-link">
+          <Link to="/user">User</Link>
+          <Link to="/post">Post</Link>
+        </div>
+      )}
+    </nav>
+  );
+};
 
 export default NavMain;
